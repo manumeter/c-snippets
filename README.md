b5425a2d6af4a7dfe6a646d85125a2b66e3b9431
manuel <manuel@deadlock.inf.ethz.ch>
2015-04-29 14:37:48 +0200

    Deadlock fix

---
+++ README.md
@@ -0,0 +1,3 @@
+# C Thread Pingpong
+
+Example in C for multithreading. To threads alternating in defined order with mutex and conditions.
---
+++ pingpong.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+int next_ping = 1;
+void *ping(), *pong();
+
+void main() {
+	pthread_t thread_ping, thread_pong;
+	pthread_cond_init(&cond, NULL);
+	
+	pthread_create(&thread_ping, NULL, ping, NULL);
+	pthread_create(&thread_pong, NULL, pong, NULL);
+	pthread_cond_signal(&cond);
+
+	pthread_join(thread_ping, NULL);
+	pthread_join(thread_pong, NULL);
+}
+
+void *ping() {
+	pthread_mutex_lock(&lock);
+	while(1) {
+		while(!next_ping) {
+			pthread_cond_wait(&cond, &lock);
+		}
+		printf("Ping ");
+		next_ping = 0;
+		pthread_cond_signal(&cond);
+	}
+	pthread_mutex_unlock(&lock);
+}
+
+void *pong() {
+	pthread_mutex_lock(&lock);
+	while(1) {
+		while(next_ping) {
+			pthread_cond_wait(&cond, &lock);
+		}
+		printf("Pong\n");
+		next_ping = 1;
+		pthread_cond_signal(&cond);
+	}
+	pthread_mutex_unlock(&lock);
+}
